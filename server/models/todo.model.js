afb5976548f2168c3ddbd831bf60c35b74145972
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-07-15 13:50:43 +0530

    changes in todoroutes

--- server/models/todo.model.js
+++ server/models/todo.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const todosSchema = mongoose.Schema({
-  todo: String,
-  iscompleted: Boolean,
-  userId: String,
+  todo: { type: String, required: true },
+  dateofCreation: { type: Date, required: true, default: Date.now() },
+  iscompleted: { type: Boolean, required: true, default: false },
 });
 
 const TodoModel = mongoose.model("Todo", todosSchema);
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,75 +1,73 @@
 const express = require("express");
-const app = express();
-const { TodoModel } = require("../Model/todo.model");
-const { Auth } = require("../middelware/middelware");
-app.use(express.json());
+
+const { TodoModel } = require("../models/todo.model");
 
 const TodoRoutes = express.Router();
-TodoRoutes.use(Auth);
-TodoRoutes.get("/", async (req, res) => {
-  console.log(req.body);
 
+// get all todos
+TodoRoutes.get("/", async (req, res) => {
   try {
-    const todos = await TodoModel.find({ userId: req.body.userId });
-    res.send(todos);
+    const todos = await TodoModel.find();
+    res.status(200).send(todos);
   } catch (err) {
-    res.send({ massage: err.message, alert: "Something went wrong" });
+    res.status(500).send(e.message);
   }
 });
 
+// get number of completed todos
+TodoRoutes.get("/completed", async (req, res) => {
+  try {
+    const todos = await TodoModel.find({ iscompleted: true });
+    res.status(200).send(todos.length);
+  } catch (err) {
+    res.status(500).send(e.message);
+  }
+});
+
+// create a new todo
 TodoRoutes.post("/create", async (req, res) => {
   let todo = req.body;
   try {
     const newtodo = await new TodoModel(todo);
     newtodo.save();
-    res.send({ massage: "created todo", alert: "todo added successfully" });
+    res
+      .status(201)
+      .send({ massage: "created todo", alert: "todo added successfully" });
   } catch (err) {
-    console.log(err);
-    res.send({ massage: err.message, alert: "Something went wrong" });
+    res.status(500).send(e.message);
   }
 });
 
+// change status of the todo
 TodoRoutes.patch("/:id", async (req, res) => {
   const id = req.params.id;
 
   try {
     const oldtodo = await TodoModel.findOne({ _id: id });
-    const useId_of_product = oldtodo.userId;
-    const userId_of_user = req.body.userId;
-    if (useId_of_product !== userId_of_user) {
-      res.send({ massage: "not Auhorised", alert: "You are not athorised" });
-    } else {
-      const status = !oldtodo.iscompleted;
 
-      const newtodo = await TodoModel.findOneAndUpdate(
-        { _id: id },
-        { iscompleted: status }
-      );
+    const status = !oldtodo.iscompleted;
 
-      res.send({ massage: "updated", alert: "Status updated successfully" });
-    }
+    await TodoModel.findOneAndUpdate({ _id: id }, { iscompleted: status });
+
+    res
+      .status(200)
+      .send({ massage: "updated", alert: "Status updated successfully" });
   } catch (err) {
-    console.log(err);
-    res.send({ massage: err.message, alert: "Something went wrong" });
+    res.status(500).send(e.message);
   }
 });
 
+// delete the todo
 TodoRoutes.delete("/:id", async (req, res) => {
   const id = req.params.id;
 
   try {
-    const oldtodo = await TodoModel.findOne({ _id: id });
-    const useId_of_product = oldtodo.userId;
-    const userId_of_user = req.body.userId;
-    if (useId_of_product !== userId_of_user) {
-      res.send({ massage: "not Auhorised", alert: "You are not athorised" });
-    } else {
-      await TodoModel.findOneAndDelete({ _id: id });
-      res.send({ massage: "deleted", alert: "todo deleted successfully" });
-    }
+    await TodoModel.findOneAndDelete({ _id: id });
+    res
+      .status(200)
+      .send({ massage: "deleted", alert: "todo deleted successfully" });
   } catch (err) {
-    console.log(err);
-    res.send({ massage: err.message, alert: "Something went wrong" });
+    res.status(500).send(e.message);
   }
 });
 module.exports = { TodoRoutes };
