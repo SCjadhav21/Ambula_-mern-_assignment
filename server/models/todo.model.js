cfd4d1df2b24ced2b6f6a360db0e7c606be203db
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-07-15 13:23:29 +0530

    adding todomodel and todo routes

--- server/models/todo.model.js
+++ server/models/todo.model.js
@@ -1,20 +1,11 @@
 const mongoose = require("mongoose");
 
-const carSchema = mongoose.Schema({
-  car_name: { type: String, required: true },
-  car_image: { type: String, required: true, unique: true },
-  colors_avl: { type: String, required: true },
-  car_price: { type: Number, required: true },
-  car_mileage: { type: Number, required: true },
-  car_desc: {
-    model: String,
-    yearOfManufacturer: String,
-    power: Number,
-    max_speed: Number,
-    priceOfNew: Number,
-  },
+const todosSchema = mongoose.Schema({
+  todo: String,
+  iscompleted: Boolean,
+  userId: String,
 });
 
-const CarModel = mongoose.model("car", carSchema);
+const TodoModel = mongoose.model("Todo", todosSchema);
 
-module.exports = { CarModel };
+module.exports = { TodoModel };
